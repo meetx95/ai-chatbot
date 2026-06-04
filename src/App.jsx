f6f2e611cfec46fa084fa9e483d708910b9acaa6
MEET <gamitmeet901@gmail.com>
2026-06-04 12:50:11 +0530

    Clean up ChatArray function by removing unused code
    Removed commented-out code for state management and SendMessage function.

--- src/App.jsx
+++ src/App.jsx
@@ -22,36 +22,15 @@ function ChatMessage({ message, sender }) {
 
 // for Save msg
 function ChatArray() {
-  // const Arrays =React.useState([
-  //   { message: "Hello", sender: "User", id: "id1" },
-  //   { message: "How can I help you!", sender: "Bot", id: "id2" },
-  //   { message: "Idk i forgot", sender: "User", id: "id3" },
-  //   { message: "its ok", sender: "Bot", id: "id4" },
-  // ]);
-  // const ChatMessage = Arrays[0];
-  // const setChatMessage = Arrays[1];
   const chats = [
     { message: "Hello", sender: "User", id: "id1" },
     { message: "How can I help you!", sender: "Bot", id: "id2" },
     { message: "Idk i forgot", sender: "User", id: "id3" },
     { message: "its ok", sender: "Bot", id: "id4" },
   ];
-  function SendMessage() {
-    ChatArray.push({
-      message: "Test", sender: "User", id: crypto.randomUUID()
-    });
-    console.log(ChatArray);
-    // setChatMessage([
-    //   ...ChatMessage,
-    //   {
-    //       message: "Test", sender: "User", id: crypto.randomUUID()
-    //   }
-    // ]);
-  }
+  
   return (
     <>
-      <button onClick={SendMessage}>Send Message</button>
-
       {chats.map((chat) => (
         <ChatMessage
           key={chat.id}
